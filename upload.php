df25a5d4edab45acea85bc8e14045e316cc2f7ce
NBComrade <NBComrade@gmail.com>
2017-02-28 17:25:09 +0300

    Addition

--- upload.php
+++ upload.php
@@ -1,8 +1,8 @@
 <?php
-    if (!empty($_FILES['file']['name'][0])) {
+    if (isset($_FILES['file']['name'][0])) {
         foreach ($_FILES['file']['name'] as $key => $value) {
-            if (move_uploaded_file($_FILES['file']['tmp_name'][$key], 'css/' . $value)) {
-                echo 'ok';
+            if (move_uploaded_file($_FILES['file']['tmp_name'][$key], 'files/' . $value)) {
+                echo 'Файлы загружены на сервер';
             }
         }
     }
